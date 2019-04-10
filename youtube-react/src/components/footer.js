3725013c0c3bc6a439ccabd75a7c8eb879fbcd3f
chinhqn <chinhsoftoneqn@gmail.com>
2019-04-10 16:34:32 +0700

    fix

--- youtube-react/src/components/footer.js
+++ youtube-react/src/components/footer.js
@@ -6,16 +6,24 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
-
+import red from '@material-ui/core/colors/red';
+import blue from '@material-ui/core/colors/blue';
+import SvgIcon from '@material-ui/core/SvgIcon';
 const styles = {
-  root: {
-    width: '100%',
-    position: 'fixed',
-    bottom: 0,
-    backgroundColor: '#81d4fa',
-  },
+    root: {
+        width: '100%',
+        position: 'fixed',
+        bottom: 0,
+        backgroundColor: '#81d4fa',
+    },
 };
-
+function HomeIcon(props) {
+    return (
+        <SvgIcon {...props}>
+            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+        </SvgIcon>
+    );
+}
 class Footer extends React.Component {
     state = {
         value: 0,
@@ -36,6 +44,7 @@ class Footer extends React.Component {
                 showLabels
                 className={classes.root}
             >
+                <BottomNavigationAction label="Home" color="secondary" icon={<HomeIcon />} />
                 <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
                 <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
                 <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
