01c3f160b4be6deda1ee5cfcc3f07b09ab57c0ea
chinhqn <chinhsoftoneqn@gmail.com>
2019-04-12 10:49:31 +0700

    remove buile foder from gitignore

---
+++ youtube-react/build/precache-manifest.5379a1447af0ae1e32dfe2d471571b36.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "258307efeaa2d9a752d8",
+    "url": "/static/js/main.0cf47efb.chunk.js"
+  },
+  {
+    "revision": "63177e7c159626d9ac1f",
+    "url": "/static/js/2.64f92752.chunk.js"
+  },
+  {
+    "revision": "258307efeaa2d9a752d8",
+    "url": "/static/css/main.57edd4ce.chunk.css"
+  },
+  {
+    "revision": "63177e7c159626d9ac1f",
+    "url": "/static/css/2.72be7c39.chunk.css"
+  },
+  {
+    "revision": "e2b488cc0d0984af609ccd51d6774200",
+    "url": "/index.html"
+  }
+];
